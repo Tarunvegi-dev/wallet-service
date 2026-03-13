466a552be36da1965eca580028f069e4b221955d
AkashSatyavarapu <akashsatyavarapu286@gmail.com>
2026-03-13 22:58:01 +0530

    Feature: User Service Initiation

--- src/main/java/com/walllet/wallet_service/user/controller/UserController.java
+++ src/main/java/com/walllet/wallet_service/user/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.walllet.wallet_service.user.controller;
 
+import com.walllet.wallet_service.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
-    
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController
+{
+    private UserService userService;
 }
---
+++ src/main/java/com/walllet/wallet_service/user/service/UserService.java
@@ -0,0 +1,6 @@
+package com.walllet.wallet_service.user.service;
+
+public class UserService
+{
+
+}
