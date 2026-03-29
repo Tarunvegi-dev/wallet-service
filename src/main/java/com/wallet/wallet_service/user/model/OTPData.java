03624cc6b5fe7e84c6e504097f5f75261db13111
AkashSatyavarapu <akashsatyavarapu286@gmail.com>
2026-03-29 22:05:43 +0530

    features: unit testing OTP Service

--- src/main/java/com/wallet/wallet_service/user/model/OTPData.java
+++ src/main/java/com/wallet/wallet_service/user/model/OTPData.java
@@ -2,16 +2,14 @@ package com.wallet.wallet_service.user.model;
 
 import java.util.Date;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class OTPData {
     String otp;
     Date otpExpiry;
---
+++ src/test/java/com/wallet/wallet_service/common/security/OTPServiceTest.java
@@ -0,0 +1,78 @@
+package com.wallet.wallet_service.common.security;
+
+import com.wallet.wallet_service.user.model.OTPData;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.modelmapper.internal.bytebuddy.matcher.ElementMatchers.any;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@Slf4j
+class OTPServiceTest {
+
+    @InjectMocks
+    private OTPService otpService;
+
+    @Test
+    void generateAndStoreOTPTest()
+    {
+        //ACT
+        String otp = otpService.generateAndStoreOTP("testUser@gmail.com");
+        log.info("Generated OTP is"+ otp);
+        //ASSERT
+        assertThat(otp).isNotNull();
+    }
+
+    @Test
+    void verifyOTPTestWithSuccessCase()
+    {
+        //ARRANGE
+        String email = "testUser@gmail.com";
+        String otp = otpService.generateAndStoreOTP("testUser@gmail.com");
+        //ACT
+        Boolean isOtpVerified = otpService.verifyOTP(email, otp);
+
+        //ASSERT
+        assertThat(isOtpVerified).isTrue();
+    }
+
+    @Test
+    void verifyOTPTestWithFailCase()
+    {
+        //ARRANGE
+        String email = "testUser1@gmail.com";
+        String email2 = "testUser2@gmail.com";
+        String otp = otpService.generateAndStoreOTP(email);
+        //ACT
+        Boolean isOtpVerified = otpService.verifyOTP(email2, otp);
+
+        //ASSERT
+        assertThat(isOtpVerified).isFalse();
+    }
+
+    @Test
+    void isOTPVerified() {
+    }
+
+    @Test
+    void clearOTP() {
+    }
+}
